a905d3ca1b463e2ce8f5339992e4d1d956aa8af3
mr0ak1 <156985564+mr0ak1@users.noreply.github.com>
2025-05-03 19:55:37 +0000

    for loop

---
+++ 05_iteretation/for_loop.js
@@ -0,0 +1,22 @@
+for(let index =0; index<10; index++){
+    const element = index;
+    console.log(element);
+}
+
+console.log("==============================================================");
+// for of loop
+// for(let i = 0;i<10;i++){
+//     console.log(i,"This is first loop");
+//     for(let j = 0;j<10;j++){
+//         console.log(j,"This is second loop");
+//         console.log(j)
+        
+        
+//     }
+// }
+
+const myArr = ["flash", "Batman","Superman"]
+for(let i=0; i<myArr.length;i++){
+    console.log(`Array item is ${myArr[i]}`);
+    
+}
