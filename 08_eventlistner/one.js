1c745196db3849d85c1b9cf81f260d282bb4e751
mr0ak1 <156985564+mr0ak1@users.noreply.github.com>
2025-05-06 19:58:16 +0000

    eventsss

--- 08_eventlistner/one.js
+++ 08_eventlistner/one.js
@@ -1,1 +1,28 @@
-//
+// document.querySelector("#owl").addEventListener('click', function(e){
+//     console.log("clicked");
+//     e.stopPropagation()
+//     e.preventDefault()
+    
+    
+// },false)
+
+document.querySelector('#owl').addEventListener('click',function(e){
+    const  val = e.target.tagName
+    console.log(val);
+    if (val ==="BUTTON"){
+        let removeit = e.target.parentNode;
+        removeit.remove()
+    }
+    
+})
+
+
+//fqalse is used for object propogation
+
+//2 type - flase means bubling means buttom to top
+// true means capturing top to bottom
+
+//type , timestamp , defaultPrevented
+//target, toelement, srcElement, currentTarget
+
+//keys- alt, shift, cntrl   
