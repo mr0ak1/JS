1fbbca97e5647f96f7551e839a3d340f121d522a
mr0ak1 <156985564+mr0ak1@users.noreply.github.com>
2025-05-12 11:41:04 +0000

    classes

---
+++ 10_classes_and_oop/myclasses.js
@@ -0,0 +1,19 @@
+class user{
+    constructor(username, email,password){
+        this.username = username
+        this.email = email
+        this.password = password
+    }
+
+    encryptPassword(){
+        return  `${this.password}abc`
+    }
+
+    changeUserName (){
+        return `${(this.username).toUpperCase()}`
+    }
+}
+
+const chai = new user("abhi","abhi@email.com",123)
+console.log(chai.encryptPassword());
+console.log(chai.changeUserName());
